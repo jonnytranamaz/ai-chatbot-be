9d763795a5ba37a333e87f4802022595afe83adb
21120569 <21120569@student.hcmus.edu.vn>
2024-09-05 10:44:13 +0700

    remove socket.io

--- ai_chatbot_be/asgi.py
+++ ai_chatbot_be/asgi.py
@@ -14,7 +14,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from api import routing
-from socketio import ASGIApp
+# from socketio import ASGIApp
 from channels.auth import AuthMiddlewareStack
 
 from django.urls import path
